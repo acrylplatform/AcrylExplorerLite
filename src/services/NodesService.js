f6ccc22270a380aaed2b89a45a3ec3c209d57bd9
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-08-20 15:00:37 +0700

    Nodes list loading fix

--- src/services/NodesService.js
+++ src/services/NodesService.js
@@ -8,38 +8,44 @@ export class NodesService extends ApiClientService {
         super(configurationService);
     }
 
-    loadNodes = () => {
+    loadNodes = (_this) => {
+        let chain = Promise.resolve();
+
         const nodes = this.configurationService.get().nodes.slice();
 
-        const promises = nodes.map((node, index) => {
+        nodes.map((node, index) => {
             const api = nodeApi(node.url);
-            return axios.all([
-                api.version(),
-                api.blocks.height(),
-                api.baseTarget(),
-                api.transactions.utxSize()
-            ]).then(axios.spread((version, height, baseTarget, unconfirmedTxCount) => {
-                const newNode = {
-                    ...node,
-                    version: version.data.version,
-                    height: height.data.height,
-                    baseTarget: baseTarget.data.baseTarget,
-                    unconfirmedTxCount: unconfirmedTxCount.data.size
-                };
-
-                return {
-                    index,
-                    node: newNode
-                };
-            }))
+            chain = chain
+                .then(() => {
+                    return new Promise((resolve, reject) => {
+                        axios.all([
+                            api.version(),
+                            api.blocks.height(),
+                            api.baseTarget(),
+                            api.transactions.utxSize()
+                        ]).then(axios.spread((version, height, baseTarget, unconfirmedTxCount) => {
+                            const newNode = {
+                                ...node,
+                                version: version.data.version,
+                                height: height.data.height,
+                                baseTarget: baseTarget.data.baseTarget,
+                                unconfirmedTxCount: unconfirmedTxCount.data.size
+                            };
+            
+                            resolve({
+                                index,
+                                node: newNode
+                            });
+                        }))
+                    })
+                })
+                .then((resolve) => {
+                    let nodes = _this.state.nodes;
+                    nodes[resolve.index] = resolve.node;
+                    _this.setState({nodes});
+                });
         });
 
-        return Promise.all(promises).then(values => {
-            values.forEach(item => {
-                nodes[item.index] = item.node;
-            });
-
-            return nodes;
-        });
+        return chain;
     }
 }

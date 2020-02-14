08e7be8b9a11493246fe86ae3d30fa8d765d7c54
Vladimir Mihaylov <zombotv82@gmail.com>
2020-02-14 14:43:38 +0700

    add code forating

--- src/faucet/Faucet.js
+++ src/faucet/Faucet.js
@@ -1,52 +1,60 @@
-import React from 'react';
-import ServiceFactory from '../services/ServiceFactory';
-import transactionMapper from '../addresses/TransactionMapper';
-import FaucetBlock from './LeftBlock';
-import config from '../configuration/config.testnet';
-import TransactionList from '../addresses/TransactionList';
+import React from "react";
+import ServiceFactory from "../services/ServiceFactory";
+import transactionMapper from "../addresses/TransactionMapper";
+import FaucetBlock from "./LeftBlock";
+import config from "../configuration/config.testnet";
+import TransactionList from "../addresses/TransactionList";
 
 export default class Faucet extends React.Component {
     state = {
         transactions: [],
-        address: config.faucetAddress,  
-    }
+        address: config.faucetAddress
+    };
     setAcryl(str) {
-        return str.replace('WAVES', 'ACRYL');
+        return str.replace("WAVES", "ACRYL");
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         const address = this.state.address;
-        try{
+        try {
             const addressService = ServiceFactory.addressService();
             const transactions = await addressService.loadTransactions(address);
-            const transactionMap = await transactionMapper(transactions, address);
+            const transactionMap = await transactionMapper(
+                transactions,
+                address
+            );
             for (const transaction of transactionMap) {
-                if(transaction.out) {
-                    transaction.out.currency = this.setAcryl(transaction.out.currency);
+                if (transaction.out) {
+                    transaction.out.currency = this.setAcryl(
+                        transaction.out.currency
+                    );
                 }
-                if(transaction.in) {
-                    transaction.in.currency = this.setAcryl(transaction.in.currency);
+                if (transaction.in) {
+                    transaction.in.currency = this.setAcryl(
+                        transaction.in.currency
+                    );
                 }
             }
             await this.setState({ transactions });
-        } catch(e) {
+        } catch (e) {
             await this.setState({ transactions: [] });
         }
-        
     }
 
     render() {
         return (
             <React.Fragment>
-                <div className='grid FuacetSection'>
-                    <div className='LeftBlock column-md-12'>
+                <div className="grid FuacetSection">
+                    <div className="LeftBlock column-md-12">
                         <FaucetBlock />
                     </div>
-                    <div className='TransactionList column-md-12'>
-                        <TransactionList transactions={this.state.transactions} address={this.state.address}/>
+                    <div className="TransactionList column-md-12">
+                        <TransactionList
+                            transactions={this.state.transactions}
+                            address={this.state.address}
+                        />
                     </div>
                 </div>
-               
             </React.Fragment>
         );
     }
--- src/faucet/LeftBlock.js
+++ src/faucet/LeftBlock.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import axios from 'axios';
-import ReCAPTCHA from 'react-google-recaptcha';
-import config from '../configuration/config.testnet';
+import React from "react";
+import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
+import config from "../configuration/config.testnet";
 
 export default class FaucetBlock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            address: '', 
-            isSubmiting:false,
-            token:'',
-            responseMessage:'',
+        this.state = {
+            address: "",
+            isSubmiting: false,
+            token: "",
+            responseMessage: ""
         };
         this.addressChange = this.addressChange.bind(this);
         this.getCoins = this.getCoins.bind(this);
         this.onChange = this.onChange.bind(this);
     }
-    
-    onChange(value){
-        if(value == null){
-            this.setState({isSubmiting: false});
-        }else{
-            this.setState({isSubmiting: true, token: value});
+
+    onChange(value) {
+        if (value == null) {
+            this.setState({ isSubmiting: false });
+        } else {
+            this.setState({ isSubmiting: true, token: value });
         }
     }
- 
-    addressChange(event){
-        this.setState({address: event.target.value});
+
+    addressChange(event) {
+        this.setState({ address: event.target.value });
     }
 
     async getCoins(event) {
         event.preventDefault();
-        try{
+        try {
             const response = await axios.post(config.getCoinsAddress, {
                 address: this.state.address,
                 token: this.state.token
-            })
-            this.setState({responseMessage:response.data})
-        } catch(e){
+            });
+            this.setState({ responseMessage: response.data });
+        } catch (e) {
             console.error(e);
         }
     }
@@ -45,24 +45,36 @@ export default class FaucetBlock extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <form className='FaucetForm' onSubmit={this.getCoins}>
+                <form className="FaucetForm" onSubmit={this.getCoins}>
                     <label>
-                        <p style={{fontWeight:"bold"}}>Testnet address</p>
+                        <p style={{ fontWeight: "bold" }}>Testnet address</p>
                         Fill out your Testnet address to receive 10 ACRYL
                     </label>
-                    <input type="text" value={this.state.address} onChange={this.addressChange} className='AdressInput'/>
-                    <label>
-                        Please enter the Captcha
-                    </label>
+                    <input
+                        type="text"
+                        value={this.state.address}
+                        onChange={this.addressChange}
+                        className="AdressInput"
+                    />
+                    <label>Please enter the Captcha</label>
                     <ReCAPTCHA
                         ref={this.recaptchaRef}
                         sitekey={config.captchaKey}
                         onChange={this.onChange}
                     />
-                    <input type="submit" value="Отправить" disabled={!this.state.isSubmiting} className='SubbmitButton'/>
-                    <span className='FaucetResponseMessage'>{this.state.responseMessage}</span><br />
+                    <input
+                        type="submit"
+                        value="Отправить"
+                        disabled={!this.state.isSubmiting}
+                        className="SubbmitButton"
+                    />
+                    <span className="FaucetResponseMessage">
+                        {this.state.responseMessage}
+                    </span>
+                    <br />
                     <span>
-                        If you experience any problems with the faucet, please contact us.
+                        If you experience any problems with the faucet, please
+                        contact us.
                     </span>
                 </form>
             </React.Fragment>
